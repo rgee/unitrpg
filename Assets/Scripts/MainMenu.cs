cd73122030732d6d6cd1285a007d7b98b064ebad
Ryan Gee <ryan.cv.gee@gmail.com>
2014-10-25 18:23:10 -0400

    Use the Unity 4.6 stuff to rebuild the main menu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,33 +3,11 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-	void OnGUI() {
+	public void StartNewGame() {
+		Application.LoadLevel("Chapter 1");
+	}
 
-		const int buttonHeight = 60;
-		const int buttonWidth = 84;
-
-		Rect buttonRect = new Rect(
-			  Screen.width / 2  - (buttonWidth / 2),
-			  (2 * Screen.height / 3) - (buttonHeight / 2),
-			  buttonWidth,
-			  buttonHeight
-			);
-
-
-		if (GUI.Button(buttonRect, "Start Game")) {
-			Application.LoadLevel("Chapter 1");
-		}
-
-
-		Rect newGameButtonRect = new Rect(
-			Screen.width / 2  - (buttonWidth / 2),
-			(int)(1.5 * Screen.height / 3) - (buttonHeight / 2),
-			buttonWidth,
-			buttonHeight
-			);
-
-		if (GUI.Button(newGameButtonRect, "Load Game")) {
-			Application.LoadLevel("LoadGame");
-		}
+	public void LoadGame() {
+		Application.LoadLevel("LoadGame");
 	}
 }
