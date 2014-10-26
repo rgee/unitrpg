aaccd993720a9d3aa3efcd499818869222fa7967
Ryan Gee <ryan.cv.gee@gmail.com>
2014-10-25 20:00:28 -0400

    Add a health bar prefab

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public GameObject clipMask;
+	public float healthPct = 100;
+
+	private RectTransform maskTransform;
+	private float initialX;
+
+	void Start () {
+		maskTransform = clipMask.GetComponent<RectTransform>();
+		initialX = maskTransform.sizeDelta.x;
+	}
+	
+	void Update () {
+		Vector2 size = maskTransform.sizeDelta;
+		maskTransform.sizeDelta = new Vector2((healthPct / 100) * initialX, size.y);
+	}
+}
