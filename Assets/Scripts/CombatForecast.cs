fe957aeb2d9f9408f84f46604ba617e03f642543
Ryan Gee <ryan.cv.gee@gmail.com>
2014-11-02 14:57:43 -0500

    Add animations to the combat forecast and a coordinator script

---
+++ Assets/Scripts/CombatForecast.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatForecast : MonoBehaviour {
+
+	GameObject attackerHalf;
+	GameObject defenderHalf;
+
+	// Use this for initialization
+	void Start () {
+		attackerHalf = transform.FindChild("Attacker").gameObject;
+		defenderHalf = transform.FindChild("Defender").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.N)) {
+			attackerHalf.GetComponent<Animator>().SetTrigger("open");
+			defenderHalf.GetComponent<Animator>().SetTrigger("open");
+		}
+	}
+}
