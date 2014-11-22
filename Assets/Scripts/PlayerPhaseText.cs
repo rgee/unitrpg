37ae4bfde77f4893eec4afc9677002a5bf8a51b2
Ryan Gee <ryan.cv.gee@gmail.com>
2014-11-22 15:28:55 -0500

    Hooked up the player phase text to trigger the next phase at the end of its animation via a script.

---
+++ Assets/Scripts/PlayerPhaseText.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhaseText : MonoBehaviour {
+
+	public BattleManager battleManager;
+
+	public void TriggerPlayerPhase() {
+		battleManager.StartPlayerPhase();
+	}
+}
