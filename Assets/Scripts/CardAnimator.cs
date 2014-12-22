c183a7297c74a3f8155a5be6740572645035912e
Ryan Gee <ryan.cv.gee@gmail.com>
2014-12-21 19:09:00 -0500

    Make the text fit in the panel and add support for delays (janky).

--- Assets/Scripts/CardAnimator.cs
+++ Assets/Scripts/CardAnimator.cs
@@ -16,15 +16,28 @@ public class CardAnimator : MonoBehaviour {
 	}
 
 	IEnumerator Animate() {
+		int sentenceIndex = 0;
 		foreach (string sentence in card.Sentences) {
+
 			foreach (char letter in sentence) {
 				textObject.text += letter;
-				yield return new WaitForSeconds(0.1f);
+				yield return new WaitForSeconds(0.02f);
 			}
 
 			textObject.text += " ";
+			if (sentenceIndex <= card.Delays.Length - 1) {
+				float delay = card.Delays[sentenceIndex];
+				if (delay > 0f) {
+					yield return new WaitForSeconds(delay);
+				} else {
+					yield return new WaitForSeconds(0.4f);
+				}
+			} else {
+				yield return new WaitForSeconds(0.4f);
+			}
 
 			Debug.Log ("Says: " + sentence);
+			sentenceIndex++;
 		}
 		complete = true;
 	}
--- Assets/Scripts/Models/Card.cs
+++ Assets/Scripts/Models/Card.cs
@@ -4,5 +4,6 @@ using System.Collections;
 namespace Models {
 	public class Card : ScriptableObject {
 		public string[] Sentences;
+		public float[] Delays;
 	}
 }
