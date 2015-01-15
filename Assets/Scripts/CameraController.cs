6e90af10bfa9314823c5ce90c42ea3a93318c414
Ryan Gee <ryan.cv.gee@gmail.com>
2015-01-14 19:13:48 -0500

    Add a grid-constricted camera component.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,12 @@ public class CameraController : MonoBehaviour {
 
 	public float speed = 10;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
 
 		float horizontalSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		float veritcalSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 		transform.Translate(new Vector3(horizontalSpeed, veritcalSpeed, 0));
+
 	}
 }
---
+++ Assets/Scripts/Grid/GridCameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridCameraController : CameraController {
+	
+	public MapGrid grid;
+
+	public void Update () {
+		base.Update();
+
+		Vector2 gridCenter = grid.transform.position;
+		float vSize = Camera.main.orthographicSize * 2.0f;
+		float hSize = vSize * ((float)Screen.width / Screen.height);
+		
+		float halfHSize = hSize * 0.5f;
+		float halfVSize = vSize * 0.5f;
+		
+		float gridHalfWidth = (grid.width/2.0f) * grid.tileSizeInPixels;
+		float maxX = gridCenter.x + gridHalfWidth;
+		float minX = gridCenter.x - gridHalfWidth;
+		
+		float leftEdge = transform.position.x - halfHSize;
+		if (leftEdge < minX) {
+			transform.position = new Vector3(minX + halfHSize, transform.position.y, transform.position.z);
+		}
+		
+		float rightEdge = transform.position.x + halfHSize;
+		if (rightEdge > maxX) {
+			transform.position = new Vector3(maxX - halfHSize, transform.position.y, transform.position.z);
+		}
+		
+		
+		float gridHalfHeight = (grid.height/2.0f) * grid.tileSizeInPixels;
+		float maxY = gridCenter.y + gridHalfHeight;
+		float minY = gridCenter.y - gridHalfHeight;
+		
+		float topEdge = transform.position.y + halfVSize;
+		if (topEdge > maxY) {
+			transform.position = new Vector3(transform.position.x, maxY - halfVSize, transform.position.z);
+		}
+		
+		float bottomEdge = transform.position.y - halfVSize;
+		if (bottomEdge < minY) {
+			transform.position = new Vector3(transform.position.x, minY + halfVSize, transform.position.z);
+		}
+	}
+}
