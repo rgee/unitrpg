747b3f1eb3fb9c17af884dff98f0d4244b99da83
Ryan Gee <ryan.cv.gee@gmail.com>
2015-01-31 11:05:05 -0500

    Fixing double deactivation of deactivated actors in cutscenes

--- Assets/Scripts/Cutscenes/CutscenePortrait.cs
+++ Assets/Scripts/Cutscenes/CutscenePortrait.cs
@@ -7,6 +7,7 @@ public class CutscenePortrait : MonoBehaviour {
 	private static Color DEACTIVATED_COLOR = new Color(0.5f, 0.5f, 0.5f);
 	private static Color ACTIVATED_COLOR = Color.white;
 	private static float TINT_DURATION_SECONDS = 0.5f;
+	private static float EPSILON = 0.1f;
 
   	[System.Serializable]
 	public class Emotion {
@@ -49,12 +50,28 @@ public class CutscenePortrait : MonoBehaviour {
 		return null;
 	}
 
+	private static bool ColorsAreSimilar(Color first, Color second) {
+		Vector3 firstVec = new Vector3(first.r, first.b, first.g);
+		Vector3 secondVec = new Vector3(second.r, second.b, second.g);
+		return (firstVec - secondVec).magnitude < EPSILON;
+	}
+
 	public void Activate() {
-		StartCoroutine(TintToColor(DEACTIVATED_COLOR, ACTIVATED_COLOR, TINT_DURATION_SECONDS));
+		// Do not attempt to activate if already activated.
+		// Use the similarity of colors as a heuristic because exact equality will likely fail by a small small
+		// margin since we lerp the colors, and Unity doesn't guarantee to end on the actual color you provide. :(
+		if (ColorsAreSimilar(Body.color, DEACTIVATED_COLOR) || ColorsAreSimilar(Head.color, DEACTIVATED_COLOR)) {
+			StartCoroutine(TintToColor(DEACTIVATED_COLOR, ACTIVATED_COLOR, TINT_DURATION_SECONDS));
+		}
 	}
 
 	public void Deactivate() {
-		StartCoroutine(TintToColor(ACTIVATED_COLOR, DEACTIVATED_COLOR, TINT_DURATION_SECONDS));
+		// Do not attempt to deactivate if already deactivated
+		// Use the similarity of colors as a heuristic because exact equality will likely fail by a small small
+		// margin since we lerp the colors, and Unity doesn't guarantee to end on the actual color you provide. :(
+		if (ColorsAreSimilar(Body.color, ACTIVATED_COLOR) || ColorsAreSimilar(Head.color, ACTIVATED_COLOR)) {
+			StartCoroutine(TintToColor(ACTIVATED_COLOR, DEACTIVATED_COLOR, TINT_DURATION_SECONDS));
+		}
 	}
 
 	private IEnumerator TintToColor(Color startColor, Color color, float time) {
