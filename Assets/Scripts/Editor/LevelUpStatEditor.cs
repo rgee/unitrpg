08b1555ca74486691079e33f376196d0da14f224
Ryan Gee <ryan.cv.gee@gmail.com>
2015-02-28 22:07:54 -0500

    Make the bursting stat into a prefab.

---
+++ Assets/Scripts/Editor/LevelUpStatEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelUpStat))]
+public class LevelUpStatEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        LevelUpStat statScript = (LevelUpStat)target;
+        if (GUILayout.Button("Increase")) {
+            statScript.Increase();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/LevelUpStat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUpStat : MonoBehaviour {
+    private GameObject text;
+    private GameObject burst;
+
+    public void Start() {
+        text = transform.FindChild("Stat Text").gameObject;
+        burst = transform.FindChild("Level Up Burst").gameObject;
+    }
+
+    public void Increase() {
+        Text statValue = text.GetComponent<Text>();
+        int number = Int32.Parse(statValue.text);
+
+        statValue.text = (number + 1).ToString();
+
+        burst.GetComponent<Animator>().SetTrigger("Burst");
+        text.GetComponent<Animator>().SetTrigger("Burst");
+    }
+}
