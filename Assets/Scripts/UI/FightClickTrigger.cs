d8a013ba70d4df0ac203335a974429ddf87d6f3a
Ryan Gee <ryan.cv.gee@gmail.com>
2015-03-08 22:03:15 -0400

    testing a fight click trigger

---
+++ Assets/Scripts/UI/FightClickTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FightClickTrigger : MonoBehaviour, IPointerClickHandler {
+
+    public BattleManager BattleManager;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        BattleManager.SelectBattleAction("fight");
+    }
+}
