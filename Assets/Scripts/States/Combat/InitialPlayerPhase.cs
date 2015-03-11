047907ae41036e4c70a678cb05fb39d7561c69f1
Ryan Gee <ryan.cv.gee@gmail.com>
2015-03-10 20:23:44 -0400

    Transition out of the initial player phase state when the user clicks on a unit.

--- Assets/Scripts/States/Combat/InitialPlayerPhase.cs
+++ Assets/Scripts/States/Combat/InitialPlayerPhase.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class InitialPlayerPhase : StateMachineBehaviour {
 
 	private Grid.UnitManager UnitManager;
+	private Animator Animator;
 	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)	{
+		Animator = animator;
 		UnitManager = GameObject.Find("Unit Manager").GetComponent<Grid.UnitManager>();
 		UnitManager.OnUnitClick += new Grid.UnitManager.UnitClickedEventHandler(OnUnitClicked);
 	}
 
 	private void OnUnitClicked(Grid.Unit unit) {
-		Debug.Log ("Unit clicked");
+		string trigger = "enemy_selected";
+		if (unit.friendly) {
+			trigger = "friendly_selected";
+		}
+
+		Animator.SetTrigger(trigger);
 	}
 
 	public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)	{
