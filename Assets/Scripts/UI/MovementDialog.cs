27cfd7b1f16b990178296da979170809587e4bb6
Ryan Gee <ryan.cv.gee@gmail.com>
2015-03-20 21:56:31 -0400

    Start building out the movement popup

---
+++ Assets/Scripts/UI/MovementDialog.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class MovementDialog : MonoBehaviour {
+	public GameObject PipPrefab;
+	public int TotalMoves;
+	public int UsedMoves;
+
+	private List<GameObject> Pips = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		GameObject pipParent = transform.FindChild("Panel/Pips").gameObject;
+		for (int i = 0; i < TotalMoves; i++) {
+			GameObject pip = Instantiate(PipPrefab) as GameObject;
+			Pips.Add(pip);
+
+			pip.transform.parent = pipParent.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI/Pip.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pip : MonoBehaviour {
+	public bool isEnabled;
+	public GameObject Enabled;
+	public GameObject Disabled;
+
+	private GameObject EnabledInstance;
+	private GameObject DisabledInstance;
+
+	// Use this for initialization
+	void Start () {
+		EnabledInstance = Instantiate(Enabled);
+		DisabledInstance = Instantiate(Disabled);
+
+		EnabledInstance.transform.parent = transform;
+		DisabledInstance.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnabledInstance.SetActive(isEnabled);
+		DisabledInstance.SetActive(!isEnabled);
+	}
+}
