8c60db2704dbe13837ba7addd3aa949eebeb5530
Ryan Gee <ryan.cv.gee@gmail.com>
2015-03-21 20:21:26 -0400

    Make a temporary game over screen and attach it to the Win battle state.

---
+++ Assets/Scripts/States/Combat/TempGameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempGameOver : StateMachineBehaviour {
+	public GameObject GameOverOverlayPrefab;
+	private GameObject GameOverOverlay;
+
+	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		GameOverOverlay = Instantiate(GameOverOverlayPrefab) as GameObject;
+	}
+
+	public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Destroy(GameOverOverlay);
+	}
+}
