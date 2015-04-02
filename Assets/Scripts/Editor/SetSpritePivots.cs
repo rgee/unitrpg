39d7edf6d2bae491988b3dc5a34d9cc636680dd3
Ryan Gee <ryan.cv.gee@gmail.com>
2015-04-02 12:32:10 -0400

    Add a menu item script to set pivots
    Will set the pivot of each cell in a sprite sheet to be the same as the
    first cell.

---
+++ Assets/Scripts/Editor/SetSpritePivots.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+class SetSpritePivots {
+    [MenuItem("Sprite/Set Pivot(s)")]
+    static void SetPivots() {
+
+        UnityEngine.Object[] textures = GetSelectedTextures();
+
+        Selection.objects = new UnityEngine.Object[0];
+        foreach (Texture2D texture in textures) {
+            string path = AssetDatabase.GetAssetPath(texture);
+            TextureImporter ti = AssetImporter.GetAtPath(path) as TextureImporter;
+            ti.isReadable = true;
+            List<SpriteMetaData> newData = new List<SpriteMetaData>();
+            for (int i = 0; i < ti.spritesheet.Length; i++) {
+                SpriteMetaData d = ti.spritesheet[i];
+                d.alignment = 9;
+                d.pivot = ti.spritesheet[0].pivot;
+                newData.Add(d);
+            }
+            ti.spritesheet = newData.ToArray();
+            AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+        }
+    }
+
+    static UnityEngine.Object[] GetSelectedTextures() {
+        return Selection.GetFiltered(typeof(Texture2D), SelectionMode.DeepAssets);
+    }
+}
