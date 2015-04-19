925e9ad599111bd8803423ad779eacdc69ba52af
Ryan Gee <ryan.cv.gee@gmail.com>
2015-04-19 13:46:41 -0400

    Add the first pass of the combat cursor.

---
+++ Assets/Scripts/UI/CombatCursor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class CombatCursor : Singleton<CombatCursor> {
+
+    private bool interactive;
+    public bool Interactive {
+        get { return interactive; }
+        set {
+            Animator.SetBool("Interactive", value);
+        }
+    }
+
+    private RectTransform Transform;
+    private Animator Animator;
+
+    public void Start() {
+        Transform = GetComponent<RectTransform>();
+        Animator = GetComponent<Animator>();
+    }
+
+    public void Update() {
+        Vector2 size = Transform.sizeDelta;
+        size.Scale(new Vector2(.5f * Transform.localScale.x, .5f * transform.localScale.y));
+
+        Transform.anchoredPosition3D = Input.mousePosition - new Vector3(size.x, size.y);
+    }
+
+    public void OnDisable() {
+        Cursor.visible = true;
+    }
+
+    public void OnEnable() {
+        Cursor.visible = false;
+    }
+}
