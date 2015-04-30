29fcca0e4a3826b713f842f201aca7e7c143279d
Ryan Gee <ryan.cv.gee@gmail.com>
2015-04-29 22:11:01 -0400

    Add the fringe highlighting

--- Assets/Scripts/States/Combat/SelectingMoveLocation.cs
+++ Assets/Scripts/States/Combat/SelectingMoveLocation.cs
@@ -17,7 +17,9 @@ public class SelectingMoveLocation : CancelableCombatState {
     private int UsedDistance;
     private Vector2 LastHoveredGridPoint;
 
+    private static readonly string PLAYER_ATTACK_PREVIEW_RANGE = "player_attack_preview_range";
     private static readonly string PLAYER_MOVE_RANGE = "player_move_range";
+    private static readonly int DEFAULT_ATTACK_RANGE = 1;
 
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		base.OnStateEnter(animator, stateInfo, layerIndex);
@@ -26,6 +28,7 @@ public class SelectingMoveLocation : CancelableCombatState {
         State = CombatObjects.GetBattleState();
 		Animator = animator;
 
+        int attackRange = DEFAULT_ATTACK_RANGE;
         int mov = State.GetRemainingDistance(State.SelectedUnit);
         UsedDistance = State.GetUsedDistance(State.SelectedUnit);
 
@@ -35,13 +38,39 @@ public class SelectingMoveLocation : CancelableCombatState {
         movingUnit.SetActive(false);
         Grid.RescanGraph();
 
+        // We show walkable locations in blue, as the range over which the unit can traverse this turn
         WalkableLocations = Grid.GetWalkableTilesInRange(State.SelectedGridPosition, mov);
+
+        // We also show the squares the unit can attack if they moved to the fringes of their move range
+        // To compute this, we find all squares within (atk_range + mov_range) of the unit, ignoring
+        // other units as obstacles.
+        // 
+        // Then, because we do not want to overlap squares here, we exclude all squares from the move range
+        // Finally, we filter out any squares that are not within atk_range of any square from the move range.
+        //
+        // This is because the unit-ignoring grid search for squares is unhindered by other units, so it's a 
+        // superset of the attackable squares a unit can reach just by walking, but we also need to include
+        // squares occupied by the enemy in the results.
+        HashSet<Vector2> AttackableLocations = Grid.GetWalkableTilesInRange(State.SelectedGridPosition, mov + attackRange, true);
+        AttackableLocations = AttackableLocations
+            .Except(WalkableLocations)
+            .Where((loc) => {
+                // Where this location is within attack range of any Walkable point.
+                return WalkableLocations.Any((walkable) => {
+                    return MathUtils.ManhattanDistance(walkable, loc) == attackRange;
+                });
+            })
+            .ToHashSet();
+
         WalkableLocations.Remove(State.SelectedGridPosition);
+        AttackableLocations.Remove(State.SelectedGridPosition);
 
         movingUnit.SetActive(true);
         Grid.RescanGraph();
 
-        MapHighlightManager.Instance.HighlightTiles(WalkableLocations, MapHighlightManager.HighlightLevel.PLAYER_MOVE, PLAYER_MOVE_RANGE);
+        MapHighlightManager highlights = MapHighlightManager.Instance;
+        highlights.HighlightTiles(AttackableLocations, MapHighlightManager.HighlightLevel.PLAYER_ATTACK, PLAYER_ATTACK_PREVIEW_RANGE);
+        highlights.HighlightTiles(WalkableLocations, MapHighlightManager.HighlightLevel.PLAYER_MOVE, PLAYER_MOVE_RANGE);
 
 		Grid.OnGridClicked += new MapGrid.GridClickHandler(HandleGridClick);
 
@@ -65,6 +94,10 @@ public class SelectingMoveLocation : CancelableCombatState {
         if (maybeMouseGridPos.HasValue) {
             Vector2 mouseGridPos = maybeMouseGridPos.Value;
             if (mouseGridPos != LastHoveredGridPoint) {
+                if (!WalkableLocations.Contains(mouseGridPos)) {
+                    return;
+                }
+
                 Grid.Unit unit = State.SelectedUnit;
                 Vector3 src = unit.transform.position;
                 Vector3 dest = Grid.GetWorldPosForGridPos(mouseGridPos);
