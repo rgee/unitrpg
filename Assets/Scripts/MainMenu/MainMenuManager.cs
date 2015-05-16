c968974da989cbcd503bc55db6f6c5dfcaa9a977
Ryan Gee <ryan.cv.gee@gmail.com>
2015-05-16 17:38:43 -0400

    Delete old main menu and lay groundwork for the new one

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public void StartNewGame() {
+
+    }
+
+    public void LoadGame() {
+
+    }
+
+    public void Quit() {
+
+    }
+}
