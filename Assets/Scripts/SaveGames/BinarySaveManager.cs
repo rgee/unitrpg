29f431c1b90d9ee572928bb01809403b3a0893b3
Ryan Gee <ryan.cv.gee@gmail.com>
2015-05-31 23:44:27 -0400

    Sketch out the first serializable framework classes

---
+++ Assets/Scripts/SaveGames/BinarySaveManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using SaveGames;
+
+namespace SaveGames {
+    public class BinarySaveManager : ISaver, ILoader {
+        public State Load(string path) {
+            var state = new State();
+            var stream = File.Open(path, FileMode.Open);
+            var binaryFormatter  = new BinaryFormatter();
+            binaryFormatter.Binder = new VersionDeserializationBinder();
+
+            var result = (State)binaryFormatter.Deserialize(stream);
+            stream.Close();
+
+            return result;
+
+        }
+
+        public void Save(State state, string path) {
+            throw new NotImplementedException();
+        }
+    }
+
+    class VersionDeserializationBinder : SerializationBinder {
+        public override Type BindToType(string assemblyName, string typeName) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/SaveGames/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SaveGames {
+    [Serializable]
+    public class State : ISerializable {
+        public State() {
+        }
+
+        public State(SerializationInfo info, StreamingContext ctx) {
+            
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+
+        }
+    }
+}
