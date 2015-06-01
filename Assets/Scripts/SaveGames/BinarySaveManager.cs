c4064b48c3189f0430b9bbda83aa6f7c9c99b510
Ryan Gee <ryan.cv.gee@gmail.com>
2015-06-01 00:34:51 -0400

    Add a savedOn time

--- Assets/Scripts/SaveGames/BinarySaveManager.cs
+++ Assets/Scripts/SaveGames/BinarySaveManager.cs
@@ -5,21 +5,23 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using SaveGames;
 
 namespace SaveGames {
     public class BinarySaveManager : ISaver, ILoader {
+        public static State CurrentState;
+
         public List<State> GetAll(string path) {
             var files = from fileName in Directory.GetFiles(path)
                         where fileName.EndsWith(".sav")
                         select fileName;
 
-            var states = from fileName in files
-                         select Load(fileName);
+            var states = (from fileName in files
+                          select Load(fileName))
+                .OrderBy((state) => state.SavedOn);
+                         
 
             return new List<State>(states);
-        } 
+        }
 
         public State Load(string path) {
             var state = new State();
@@ -35,6 +37,7 @@ namespace SaveGames {
         public void Save(State state, string path) {
             var stream = File.Open(path, FileMode.Create);
             var binaryFormatter = new BinaryFormatter {Binder = new VersionDeserializationBinder()};
+            state.SavedOn = new DateTime();
             binaryFormatter.Serialize(stream, state);
             stream.Close();
         }
@@ -47,7 +50,7 @@ namespace SaveGames {
             }
 
             assemblyName = Assembly.GetExecutingAssembly().FullName;
-            return Type.GetType(String.Format("{0}, {1}", typeName, assemblyName));
+            return Type.GetType(string.Format("{0}, {1}", typeName, assemblyName));
         }
     }
 }
--- Assets/Scripts/SaveGames/State.cs
+++ Assets/Scripts/SaveGames/State.cs
@@ -7,6 +7,7 @@ using System.Text;
 namespace SaveGames {
     [Serializable]
     public class State : ISerializable {
+        public DateTime SavedOn;
         public ulong SecondsPlayed;
         public int Chapter;
         public List<Character> Characters;
@@ -18,12 +19,14 @@ namespace SaveGames {
             Chapter = info.GetInt32("chapter");
             SecondsPlayed = info.GetUInt64("secondsPlayed");
             Characters = (List<Character>) info.GetValue("characters", typeof (List<Character>));
+            SavedOn = info.GetDateTime("savedOn");
         }
 
         protected virtual void GetObjectData(SerializationInfo info, StreamingContext context) {
             info.AddValue("chapter", Chapter);
             info.AddValue("secondsPlayed", SecondsPlayed);
             info.AddValue("characters", Characters);
+            info.AddValue("savedOn", SavedOn);
         }
 
         void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context) {
