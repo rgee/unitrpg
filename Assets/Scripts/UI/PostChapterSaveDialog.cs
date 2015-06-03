25b5e3b486e141540f2b8b6732952c12ec127ab4
Ryan Gee <ryan.cv.gee@gmail.com>
2015-06-02 21:20:43 -0400

    Add a scene to give the player the option to save between chapters.

---
+++ Assets/Scripts/UI/PostChapterSaveDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class PostChapterSaveDialog : MonoBehaviour {
+    public void GoToSaveScene() {
+       Application.LoadLevel("SaveGame"); 
+    }
+
+    public void SkipSaving() {
+        var chapterNumber = SaveGame.current.chapter;
+        var sceneName = string.Format("chapter_{0}_intro", chapterNumber);
+
+        Application.LoadLevel(sceneName);
+    }
+}
