5efd71453356e6dc81072e126b68e913a8ef950e
Ryan Gee <ryan.cv.gee@gmail.com>
2015-06-20 08:16:50 -0400

    Add the new map and dynamic sorted object.

---
+++ Assets/Scripts/DynamicSortedObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DynamicSortedObject : MonoBehaviour {
+    public float GridSize;
+
+    private Renderer _renderer;
+
+    void Start() {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        _renderer.sortingOrder = -Mathf.RoundToInt(transform.position.y/GridSize);
+    }
+}
