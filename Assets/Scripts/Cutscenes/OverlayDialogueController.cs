ffad20a6e365b8ab5535d259160c6b917b1a8498
Ryan Gee <ryan.cv.gee@gmail.com>
2015-10-11 18:11:15 -0400

    Abolish inheritance structure from dialogues

---
+++ Assets/Scripts/Cutscenes/OverlayDialogueController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using Models.Dialogue;
+using UnityEngine;
+
+public class OverlayDialogueController : MonoBehaviour, IDialogueController {
+    private DialoguePortraitView _portraitView;
+    private string _currentSpeakerName;
+
+    private void Awake() {
+        _portraitView = transform.FindChild("Portrait View").GetComponent<DialoguePortraitView>();
+    }
+
+    public void ChangeSpeaker(string speaker) {
+        _currentSpeakerName = speaker;
+    }
+
+    public void ChangeEmotion(string speaker, EmotionalResponse response) {
+        // The overlay only updates the actor currently speaking, and they always face left.
+        if (speaker == _currentSpeakerName) {
+            _portraitView.SetActor(speaker, response.emotion, Facing.Left);
+        }
+    }
+
+    public IEnumerator Initialize(Cutscene model) {
+        yield return null;
+    }
+
+    public IEnumerator End() {
+        Destroy(gameObject);
+        yield return null;
+    }
+}
