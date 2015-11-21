a0126a65c48a79f1a5b0ce513c18f8c82917a6f4
Ryan Gee <ryan.cv.gee@gmail.com>
2015-11-21 15:22:19 -0500

    Add an interactive dialogue

---
+++ Assets/Scripts/Sequencing/Events/Dialogue/ShowDialogue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using WellFired;
+
+namespace Assets.Sequencing.Events.Dialogue {
+    [USequencerFriendlyName("Interactive Dialogue")]
+    [USequencerEvent("Dialogue/ShowInteractiveDialogue")]
+    public class ShowDialogue : USEventBase {
+        public GameObject Dialogue;
+        public override void FireEvent() {
+            Sequence.Pause();
+
+            var dialogue = Dialogue.GetComponent<global::Dialogue>();
+            dialogue.OnComplete += OnDialogueComplete;
+            dialogue.Begin();
+        }
+
+        public override void ProcessEvent(float runningTime) {
+        }
+
+        private void OnDialogueComplete() {
+            Sequence.Play();
+        }
+    }
+}
