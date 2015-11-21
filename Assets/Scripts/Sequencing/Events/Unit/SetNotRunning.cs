fee993bfceb4e0470cc307a89e24f80f8ac5917a
Ryan Gee <ryan.cv.gee@gmail.com>
2015-11-21 13:57:54 -0500

    Add a test scene that uses uSequencer for intro cutscenes.

---
+++ Assets/Scripts/Sequencing/Events/Unit/SetNotRunning.cs
@@ -0,0 +1,17 @@
+﻿using WellFired;
+
+namespace Assets.Sequencing.Events.Unit {
+    [USequencerFriendlyName("Set Not Running")]
+    [USequencerEvent("Unit/SetNotRunning")]        
+    [USequencerEventHideDuration]
+    public class SetNotRunning : USEventBase{
+         
+        public override void FireEvent() {
+            var unit = AffectedObject.GetComponent<Grid.Unit>();
+            unit.Running = false;
+        }
+
+        public override void ProcessEvent(float runningTime) {
+        }
+    }
+}
