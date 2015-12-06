67d955e19bf894be47eed6fdb15409298c358136
Ryan Gee <ryan.cv.gee@gmail.com>
2015-12-06 17:39:33 -0500

    Add a generic DoTween backed move event

---
+++ Assets/Scripts/Sequencing/Events/Transform/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using WellFired;
+
+namespace Sequencing.Events.Transform {
+    [USequencerFriendlyName("Move To")]
+    [USequencerEvent("Transform/Move To")]        
+    [USequencerEventHideDuration]
+    [ExecuteInEditMode]
+    public class MoveTo : USEventBase {
+        public Vector3 Destination;
+        public Ease Easing = Ease.OutCubic;
+
+        public override void FireEvent() {
+            AffectedObject.transform.DOLocalMove(Destination, Duration).SetEase(Easing);
+        }
+
+        public override void ProcessEvent(float runningTime) {
+
+        }
+    }
+}
