d5b11028e74c2dcd800b7c1b929e91dbe2965b55
Ryan Gee <ryan.cv.gee@gmail.com>
2015-12-26 21:46:23 -0500

    Add a script to trigger grid based manually-interacted events

---
+++ Assets/Scripts/Combat/Interactables/InteractiveSquare.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Combat.Interactables {
+    [RequireComponent(typeof(IScriptedEvent))]
+    public class InteractiveSquare : MonoBehaviour {
+        [Tooltip("Whether this object has already been interacted with.")]
+        public bool Triggered;
+
+        [Tooltip("Whether this object can be interacted with repeatedly.")]
+        public bool Repeatable;
+
+        [Tooltip("The position of this square in grid space.")]
+        public Vector2 GridPosition;
+
+        private IScriptedEvent _event;
+
+        private void Awake() {
+            _event = GetComponent<IScriptedEvent>();
+        }
+
+        public IEnumerator Trigger() {
+            // If it's a repeatable interaction, just let it go
+            // Otherwise, only activate if it hasn't already been activated, and mark that it's been activated.
+            if (!Repeatable) {
+                if (Triggered) {
+                    yield return null;
+                } else {
+                    yield return StartCoroutine(_event.Play());
+                    Triggered = true;
+                }
+            } else {
+                yield return StartCoroutine(_event.Play());
+            }
+        }
+    }
+}
