85796a707162b1ada8afa0d2c793b0a6da882de1
Ryan Gee <ryan.cv.gee@gmail.com>
2015-12-28 16:59:36 -0500

    Add a reusable scripted event to start a dialogue

---
+++ Assets/Scripts/Combat/ScriptedEvents/StartDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using Combat;
+using UnityEngine;
+
+namespace ScriptedEvents {
+    public class StartDialogue : MonoBehaviour, IScriptedEvent {
+        public GameObject DialoguePrefab;
+
+        public IEnumerator Play() {
+            var dialogueObject = Instantiate(DialoguePrefab);
+            var dialogue = dialogueObject.GetComponent<Dialogue>();
+
+            var completed = false;
+            dialogue.OnComplete += () => {
+                completed = true;
+            };
+
+            dialogue.Begin();
+            while (!completed) {
+                yield return null;
+            }
+
+            Destroy(dialogueObject);
+        }
+    }
+}
