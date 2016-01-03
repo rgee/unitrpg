b75e6875767255fae15314ba397b265645cc3ea2
Ryan Gee <ryan.cv.gee@gmail.com>
2016-01-03 16:12:26 -0500

    Create a reinforcement group object and make the chapter 2 bunch

---
+++ Assets/Scripts/Combat/Reinforcements.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Combat.ScriptedEvents;
+using UnityEngine;
+
+namespace Combat {
+    public class Reinforcements : ScriptableObject {
+        public List<SpawnableUnit> Units;
+    }
+}
