a30afb41c3c51f6fa7c6cc4621197408b28909e9
Ryan Gee <ryan.cv.gee@gmail.com>
2016-01-03 16:52:55 -0500

    Stub out scripting for visits in chater 2

---
+++ Assets/Scripts/Combat/ScriptedEvents/Chapter2/HouseVisit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using Combat;
+
+namespace Assets.Combat.ScriptedEvents.Chapter2 {
+    public class HouseVisit : IScriptedEvent {
+        public IEnumerator Play() {
+            // Play the dialogue
+            // Turn out the lights
+            // Remove the interactive tile
+            yield return null;
+        }
+    }
+}
