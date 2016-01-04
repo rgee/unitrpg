cf3a0b21051dfc483c8021a35c44c860018ee156
Ryan Gee <ryan.cv.gee@gmail.com>
2016-01-03 19:53:29 -0500

    Add more lights and allow them to start disable

--- Assets/Scripts/Combat/Props/HouseLight.cs
+++ Assets/Scripts/Combat/Props/HouseLight.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 namespace Combat.Props {
     public class HouseLight : MonoBehaviour, IToggleableProp {
         private tk2dSprite _sprite;
+        private Vector3 _originalScale;
+        public bool Enabled;
 
         private void Awake() {
             _sprite = GetComponent<tk2dSprite>();
+            _originalScale = _sprite.scale;
+
+            if (!Enabled) {
+                _sprite.scale = new Vector3(0, 0, 1);
+            }
         }
 
         public IEnumerator Enable() {
-            yield return _sprite.DOScale(new Vector3(1, 1, 1), 0.7f).WaitForCompletion();
+            yield return _sprite.DOScale(_originalScale, 0.7f).WaitForCompletion();
+            Enabled = true;
         }
 
         public IEnumerator Disable() {
             yield return _sprite.DOScale(new Vector3(0, 0, 1), 0.7f).WaitForCompletion();
+            Enabled = false;
         }
     }
 }
