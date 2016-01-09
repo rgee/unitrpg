c6d3312fe36af5aa9763176ec6ee1a2b644e5394
Ryan Gee <ryan.cv.gee@gmail.com>
2016-01-09 14:28:35 -0500

    Add zoom in uSequencer events

---
+++ Assets/Scripts/Sequencing/Events/tk2D/ResetToStandardZoom.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using WellFired;
+
+namespace Sequencing.Events.tk2D {
+    [USequencerFriendlyName("Reset Zoom")]
+    [USequencerEvent("tk2D/Camera/ResetZoom")]
+    public class ResetToStandardZoom : USEventBase {
+        public override void FireEvent() {
+            var camera = AffectedObject.GetComponent<tk2dCamera>();
+            DOTween.To(() => camera.ZoomFactor, zf => camera.ZoomFactor = zf, 1.89f, Duration)
+                .SetEase(Ease.OutCubic);
+        }
+
+        public override void ProcessEvent(float runningTime) {
+        }
+    }
+}
---
+++ Assets/Scripts/Sequencing/Events/tk2D/ZoomTo.cs
@@ -0,0 +1,19 @@
+﻿using DG.Tweening;
+using WellFired;
+
+namespace Sequencing.Events.tk2D {
+    [USequencerFriendlyName("Zoom To")]
+    [USequencerEvent("tk2D/Camera/ZoomTo")]
+    public class ZoomTo : USEventBase {
+        public float NewZoom;
+
+        public override void FireEvent() {
+            var camera = AffectedObject.GetComponent<tk2dCamera>();
+            DOTween.To(() => camera.ZoomFactor, zf => camera.ZoomFactor = zf, NewZoom, Duration)
+                .SetEase(Ease.OutCubic);
+        }
+
+        public override void ProcessEvent(float runningTime) {
+        }
+    }
+}
