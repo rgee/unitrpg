aafd1da96d19c1e2fce56dbc0b75f9bc327011f5
Ryan Gee <ryan.cv.gee@gmail.com>
2016-02-13 16:27:24 -0500

    Sketch out skill result collaction

--- Assets/Scripts/Models/Fighting/FightSimulator.cs
+++ Assets/Scripts/Models/Fighting/FightSimulator.cs
@@ -16,6 +16,8 @@ namespace Models.Fighting {
         public FightPreview Simulate(ICombatant attacker, ICombatant defender, ISkillStrategy skillStrategy) {
             var distance = MathUtils.ManhattanDistance(attacker.Position, defender.Position);
             var firstAttack = skillStrategy.Compute(attacker, defender, _randomizer);
+            var flankerAttack = null as SkillResult;
+            var doubleAttack = null as SkillResult;
 
             if (skillStrategy.SupportsFlanking) {
                 var flankerPosition = MathUtils.GetPositionAcrossFight(attacker.Position);
@@ -24,16 +26,56 @@ namespace Models.Fighting {
                 ICombatant flanker = null; //_map.GetUnitByPosition(flankerPosition);
                 if (flanker != null) {
                     var flankStrategy = flanker.GetStrategyByDistance(distance);
-                    var flankerAttack = flankStrategy.Compute(attacker, defender, _randomizer);
+                    flankerAttack = flankStrategy.Compute(attacker, defender, _randomizer);
                 }
             }
 
             var counterStrategy = defender.GetStrategyByDistance(distance);
-            var counterAttack = counterStrategy.Compute(defender, attacker, _randomizer);
-            if (skillStrategy.DidDouble(attacker, defender)) {
-                var doubleAttack = skillStrategy.Compute(attacker, defender, _randomizer);
+            var counterAttack = null as SkillResult;
+            if (counterStrategy != null) {
+                counterAttack = counterStrategy.Compute(defender, attacker, _randomizer);
             }
-            
+
+            if (skillStrategy.DidDouble(attacker, defender)) {
+                doubleAttack = skillStrategy.Compute(attacker, defender, _randomizer);
+            }
+
+
+            // Defender dies on the first hit
+            var defenderHealth = defender.Health;
+            var firstAttackDamage = firstAttack.GetDefenderDamage();
+            var totalDamage = firstAttackDamage;
+            if (firstAttackDamage >= defenderHealth) {
+               // Bail out with just the first attack 
+            }
+
+            if (flankerAttack != null) {
+                var flankDamage = flankerAttack.GetDefenderDamage();
+                totalDamage += flankDamage;
+                if (totalDamage >= defenderHealth) {
+                    // Bail out with two attacks
+                }
+            }
+
+            var attackerHealth = attacker.Health;
+            var totalAttackerDamage = 0;
+            if (counterAttack != null) {
+                // Attacker dies after being countered
+                totalAttackerDamage = counterAttack.GetDefenderDamage();
+                if (totalAttackerDamage >= attackerHealth) {
+                    // Bail out with 1-2 initial attacks and the defender attack
+                }
+            }
+
+            if (doubleAttack != null) {
+                var doubleDamage = doubleAttack.GetDefenderDamage();
+                totalAttackerDamage += doubleDamage;
+                if (totalAttackerDamage >= attackerHealth) {
+                    // Bail out with all previous hits + the double
+                }
+            }
+
+            // Bail out with just the first hit
             return null;
         }
         
--- Assets/Scripts/Models/Fighting/Skills/SkillResult.cs
+++ Assets/Scripts/Models/Fighting/Skills/SkillResult.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Models.Fighting.Effects;
 
 namespace Models.Fighting.Skills {
-    public struct SkillResult {
+    public class SkillResult {
         public readonly List<IEffect> InitiatorEffects;
         public readonly List<IEffect> ReceiverEffects;
 
         public SkillResult(List<IEffect> initiatorEffects, List<IEffect> receiverEffects) {
             InitiatorEffects = initiatorEffects;
             ReceiverEffects = receiverEffects;
+        }
+
+        public int GetAttackerDamage() {
+            return InitiatorEffects.OfType<Damage>().Sum(dmgEffect => dmgEffect.Amount);
+        }
+
+        public int GetDefenderDamage() {
+            return ReceiverEffects.OfType<Damage>().Sum(dmgEffect => dmgEffect.Amount);
         }
     }
 }
