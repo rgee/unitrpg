704b6c7b591960825e4d930f99e320c4dbbeba23
Ryan Gee <ryan.cv.gee@gmail.com>
2016-02-22 21:52:14 -0500

    Fix indentation in BaseCharacterTest

--- Assets/Scripts/Editor/Tests/Character/BaseCharacterTest.cs
+++ Assets/Scripts/Editor/Tests/Character/BaseCharacterTest.cs
@@ -4,39 +4,39 @@ using NUnit.Framework;
 using WellFired.Shared;
 
 namespace Tests.Character {
-        [TestFixture]
-        public class BaseCharacterTest {
-            [Test]
-            public void TestLeveling() {
-                var underTest = new CharacterBuilder()
-                    .Growths(new GrowthsBuilder()
-                        .Health(15)
-                        .Skill(12)
-                        .Defense(2)
-                        .Special(0)
-                        .Speed(13)
-                        .Strength(7)
+    [TestFixture]
+    public class BaseCharacterTest {
+        [Test]
+        public void TestLeveling() {
+            var underTest = new CharacterBuilder()
+                .Growths(new GrowthsBuilder()
+                    .Health(15)
+                    .Skill(12)
+                    .Defense(2)
+                    .Special(0)
+                    .Speed(13)
+                    .Strength(7)
                     .Build())
-                    .Attributes(new AttributesBuilder()
-                        .Health(1)
-                        .Skill(1)
-                        .Defense(1)
-                        .Special(1)
-                        .Speed(1)
-                        .Strength(1)
+                .Attributes(new AttributesBuilder()
+                    .Health(1)
+                    .Skill(1)
+                    .Defense(1)
+                    .Special(1)
+                    .Speed(1)
+                    .Strength(1)
                     .Build())
-                    .Weapons("Shortsword")
-                    .Build();
-                var randomizer = new ConstantRandomizer(0);
+                .Weapons("Shortsword")
+                .Build();
+            var randomizer = new ConstantRandomizer(0);
 
-                underTest.LevelUp(randomizer);
-                var newAttrs = underTest.Attributes;
-                newAttrs.Each((attr) => {
-                    Assert.AreEqual(2, attr.Value);
-                });
+            underTest.LevelUp(randomizer);
+            var newAttrs = underTest.Attributes;
+            newAttrs.Each((attr) => {
+                Assert.AreEqual(2, attr.Value);
+            });
 
-                Assert.AreEqual(2, underTest.Level);
-            }
+
+            Assert.AreEqual(2, underTest.Level);
         }
     }
 }
