266ac7ee7f5f8df95fd0852c2052e4f5cf10a592
Ryan Gee <ryan.cv.gee@gmail.com>
2016-02-28 14:21:43 -0500

    Implement FIghtForecastr

--- Assets/Scripts/Models/Fighting/Execution/FightForecaster.cs
+++ Assets/Scripts/Models/Fighting/Execution/FightForecaster.cs
@@ -1,11 +1,51 @@
-﻿using Models.Fighting.Skills;
+﻿using System.Linq;
+using Models.Fighting.Skills;
 
 namespace Models.Fighting.Execution {
     public class FightForecaster {
         public FightForecast Forecast(ICombatant attacker, ICombatant defender, SkillType type) {
             var strategy = SkillDatabase.Instance.GetStrategyByType(type);
+            var firstSkill = strategy.Forecast(attacker, defender);
+            var flankForecast = null as SkillForecast;
 
-            return null;
+            if (strategy.SupportsFlanking) {
+                var flankerPosition = MathUtils.GetPositionAcrossFight(attacker.Position);
+                var flankerDistance = MathUtils.ManhattanDistance(defender.Position, flankerPosition);
+
+                // TODO: Convert to ICombatant
+                var flanker = null as ICombatant; //_map.GetUnitByPosition(flankerPosition);
+                if (flanker != null) {
+                    var flankerStrategy = ChooseStrategyByDistance(flanker, flankerDistance);
+                    flankForecast = flankerStrategy.Forecast(flanker, defender);
+                }
+            }
+
+            var distance = MathUtils.ManhattanDistance(attacker.Position, defender.Position);
+            var counterStrategy = ChooseStrategyByDistance(defender, distance);
+            var defenderforecast = null as SkillForecast;
+            if (counterStrategy != null) {
+                defenderforecast = counterStrategy.Forecast(defender, attacker);
+            }
+
+            return new FightForecast {
+                AttackerForecast = firstSkill,
+                DefenderForecast = defenderforecast,
+                FlankerForecast = flankForecast
+            };
+        }
+
+        private ISkillStrategy ChooseStrategyByDistance(ICombatant combatant, int distance) {
+            var appropriateWeapon = combatant.EquippedWeapons.FirstOrDefault(weapon => weapon.Range == distance);
+
+            if (appropriateWeapon == null) {
+                return null;
+            }
+
+            if (distance == 1) {
+                return new MeleeAttack();
+            } else {
+                return new ProjectileAttack();
+            }
         }
     }
 }
