5afe56881dc8b3299fa80f17e387f0f5030728b2
Ryan Gee <ryan.cv.gee@gmail.com>
2016-02-29 17:39:23 -0500

    Delete fight executor and stub out fight finalizer

---
+++ Assets/Scripts/Models/Fighting/Execution/FightFinalizer.cs
@@ -0,0 +1,7 @@
+﻿namespace Models.Fighting.Execution {
+    public class FightFinalizer {
+        public ComputedFight Finalize(FightForecast forecast) {
+            return new ComputedFight();
+        }
+    }
+}
