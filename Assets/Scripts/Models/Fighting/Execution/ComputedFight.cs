00c1fdbf4bd815989d8f0a285007ab2c3b27aaa6
Ryan Gee <ryan.cv.gee@gmail.com>
2016-02-29 17:34:35 -0500

    Add models for describing the post-randomization stage of a fight

---
+++ Assets/Scripts/Models/Fighting/Execution/ComputedFight.cs
@@ -0,0 +1,26 @@
+﻿namespace Models.Fighting.Execution {
+    /// <summary>
+    /// This is a fight that has already been rolled for, and everything has been computed.
+    /// </summary>
+    public class ComputedFight {
+        /// <summary>
+        /// The phase that initiates the fight
+        /// </summary>
+        public ComputedFightPhase InitialPhase { get; set; }
+
+        /// <summary>
+        /// The phase where the flanker attacks the target too. (optional)
+        /// </summary>
+        public ComputedFightPhase FlankerPhase { get; set; }
+
+        /// <summary>
+        /// The phase where the receiver delivers a counter action. (optional)
+        /// </summary>
+        public ComputedFightPhase CounterPhase { get; set; }
+
+        /// <summary>
+        /// The phase where the initiator attacks again. (optional)
+        /// </summary>
+        public ComputedFightPhase DoubleAttackPhase { get; set; }
+    }
+}
---
+++ Assets/Scripts/Models/Fighting/Execution/ComputedFightPhase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Models.Fighting.Skills;
+
+namespace Models.Fighting.Execution {
+    /// <summary>
+    /// The effects of a single phase of a fight. 
+    /// e.g. in a fight where there is one attack, and one counterattack, there are two phases.
+    /// </summary>
+    public class ComputedFightPhase {
+        /// <summary>
+        /// The combatant who acts during this phase.
+        /// </summary>
+        public ICombatant Initiator { get; set; }
+
+        /// <summary>
+        /// The combatant who receives the action.
+        /// </summary>
+        public ICombatant Receiver { get; set; }
+
+        /// <summary>
+        /// The receiver's response to the action in this phase.
+        /// </summary>
+        public DefenderResponse Response { get; set; }
+
+        /// <summary>
+        /// The thing(s) that happen(s) to the receiver after this fight phase.
+        /// </summary>
+        public List<IEffect> Effects { get; set; }
+
+        /// <summary>
+        /// The skill that was used by the initiator.
+        /// </summary>
+        public SkillType Skill { get; set; }
+    }
+}
