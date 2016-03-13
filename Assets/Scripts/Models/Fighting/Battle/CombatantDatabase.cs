2fbe0a978f61469384f484de254fe60e16df6e30
Ryan Gee <ryan.cv.gee@gmail.com>
2016-03-13 12:16:57 -0400

    Implement combatant database

---
+++ Assets/Scripts/Models/Fighting/Battle/CombatantDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Models.Fighting.Characters;
+
+namespace Models.Fighting.Battle {
+    public class CombatantDatabase : ICombatantDatabase {
+        private readonly ILookup<ArmyType, ICombatant> _combatants; 
+
+        public CombatantDatabase(List<ICombatant> combatants) {
+            _combatants = combatants.ToLookup(c => c.Army, c => c);
+        }
+
+        public List<ICombatant> GetCombatantsByArmy(ArmyType army) {
+            return _combatants[army].ToList();
+        }
+    }
+}
