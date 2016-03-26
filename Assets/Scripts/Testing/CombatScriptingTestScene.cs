c926c87886eadf1e27c8791339ee3d3daed9cf3b
Ryan Gee <ryan.cv.gee@gmail.com>
2016-03-26 15:14:19 -0400

    Make a test scene for combat scripting

---
+++ Assets/Scripts/Testing/CombatScriptingTestScene.cs
@@ -0,0 +1,12 @@
+﻿using Grid;
+using UnityEngine;
+
+namespace Assets.Testing {
+    public class CombatScriptingTestScene : MonoBehaviour {
+        private UnitManager _unitManager;
+
+        void Awake() {
+            _unitManager = CombatObjects.GetUnitManager();
+        }
+    }
+}
