01d68a99e7d14e3e383c92995bf654be73a8ad18
Ryan Gee <ryan.cv.gee@gmail.com>
2016-04-02 10:34:34 -0400

    Add a test scene runner to show the forecast

---
+++ Assets/Scripts/Testing/CombatForecastTestScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using Models.Fighting.Execution;
+using UI.CombatForecast;
+using UnityEngine;
+
+namespace Assets.Testing {
+    public class CombatForecastTestScene : MonoBehaviour {
+        void Start() {
+            StartCoroutine(SetupScene());
+        }
+
+        IEnumerator SetupScene() {
+            yield return new WaitForSeconds(1);
+
+            var forecastManager = CombatForecastManager.Instance;
+            var fightForecast = null as FightForecast;
+
+            forecastManager.ShowForcast(fightForecast);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/CombatForecast/CombatForecastManager.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using DG.Tweening;
+using Models.Fighting.Execution;
+using UnityEngine;
+
+namespace UI.CombatForecast {
+    public class CombatForecastManager : Singleton<CombatForecastManager> {
+        public event Action OnConfirm;
+        public event Action OnReject;
+
+        public GameObject ForecastPrefab;
+
+        private GameObject _currentWindow;
+
+        void Awake() {
+            CombatEventBus.Backs.AddListener(HideForecast);
+        }
+
+        public void ShowForcast(FightForecast forecast) {
+            _currentWindow = Instantiate(ForecastPrefab);
+            _currentWindow.transform.SetParent(transform);
+
+            var forecastWindow = _currentWindow.transform.FindChild("Forecast");
+            var canvasGroup = forecastWindow.GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 0;
+
+            forecastWindow.transform.localPosition = new Vector3(0, -40, 0);
+
+            var animSequence = DOTween.Sequence();
+
+            animSequence.Insert(0, canvasGroup.DOFade(1, 0.3f));
+            animSequence.Insert(0, forecastWindow.transform.DOLocalMove(Vector3.zero, 0.3f));
+
+            animSequence.Play();
+        }
+
+        public void HideForecast() {
+            if (_currentWindow == null) {
+                return;
+            } 
+
+
+        }
+    }
+}
