37f33f12e881758cd0290d998b4097ff54791dbd
Ryan Gee <ryan.cv.gee@gmail.com>
2016-04-17 16:28:06 -0400

    Prevent shove from taking effect if the destination is blocked

--- Assets/Scripts/Editor/Tests/Battle/BattleTest.cs
+++ Assets/Scripts/Editor/Tests/Battle/BattleTest.cs
@@ -108,6 +108,45 @@ namespace Tests.Battle {
         }
 
         [Test]
+        public void TestObstructedKnockback() {
+            
+            var map = new Map();
+            var random = new BasicRandomizer();
+            var turnOrder = new List<ArmyType> { ArmyType.Friendly, ArmyType.Enemy, ArmyType.Other };
+            var references = new List<CombatantDatabase.CombatantReference> {
+                new CombatantDatabase.CombatantReference {
+                    Position = new Vector2(),
+                    Name = "Liat",
+                    Army = ArmyType.Friendly
+                },
+                new CombatantDatabase.CombatantReference {
+                    Position = new Vector2(1, 0),
+                    Name = "Janek",
+                    Army = ArmyType.Enemy
+                }
+            };
+
+            var database = new CombatantDatabase(references, new EmptySaveGameRepository());
+            var battle = new Models.Fighting.Battle.Battle(map, random, database, turnOrder);
+
+            var liat = database.GetCombatantsByArmy(ArmyType.Friendly)[0];
+            var janek = database.GetCombatantsByArmy(ArmyType.Enemy)[0];
+            map.AddObstruction(new Vector2(1, 2));
+
+            var moveAction = new MoveAction(map, liat, new Vector2(1, 1));
+            battle.SubmitAction(moveAction);
+            Assert.AreEqual(new Vector2(1, 1), liat.Position);
+
+            var forecast = battle.ForecastFight(janek, liat, SkillType.Knockback);
+            var finalizedFight = battle.FinalizeFight(forecast);
+
+            var fightAction = new FightAction(liat, janek, finalizedFight);
+            battle.SubmitAction(fightAction);
+
+            Assert.AreEqual(liat.Position, new Vector2(1, 1));
+        }
+
+        [Test]
         public void TestAdvanceNoKill() {
             var map = new Map();
             var random = new BasicRandomizer();
