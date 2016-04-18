815cb0adffb1bd1c73ed4e9418b479100d777a4c
Ryan Gee <ryan.cv.gee@gmail.com>
2016-04-18 18:01:03 -0400

    Add script for test scene

---
+++ Assets/Scripts/Testing/ShoveCombatScriptingTestScene.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Combat;
+using Grid;
+using Models.Fighting;
+using Models.Fighting.Characters;
+using Models.Fighting.Effects;
+using Models.Fighting.Execution;
+using Models.Fighting.Maps;
+using Models.Fighting.Skills;
+using UnityEngine;
+
+namespace Assets.Testing {
+    public class ShoveCombatScriptingTestScene : MonoBehaviour {
+        private FightPhase _firstPhase;
+        private FightPhaseAnimator _phaseAnimator;
+        private UnitManager _unitManager;
+
+        void Awake() {
+
+            var janekStats = new CharacterBuilder()
+                .Id("janek")
+                .Name("Janek")
+                .Attributes(new AttributesBuilder()
+                    .Move(5)
+                    .Health(15)
+                    .Skill(12)
+                    .Defense(2)
+                    .Special(0)
+                    .Speed(13)
+                    .Strength(7)
+                    .Build())
+                .Weapons("Chained Mace")
+                .Build();
+
+            var gatsuStats = new CharacterBuilder()
+                .Id("Gatsu")
+                .Name("Soldier")
+                .Attributes(new AttributesBuilder()
+                    .Health(1)
+                    .Skill(12)
+                    .Move(6)
+                    .Defense(2)
+                    .Special(0)
+                    .Speed(13)
+                    .Strength(7)
+                .Build())
+                .Stats(new StatsBuilder()
+                    .ParryChance(0)
+                .Build())
+                .Weapons("Chained Mace")
+                .Build();
+
+            var janek = new BaseCombatant(janekStats, ArmyType.Friendly);
+            var gatsu = new BaseCombatant(gatsuStats, ArmyType.Enemy);
+
+            var map = new Map();
+
+            _firstPhase = new FightPhase {
+                Initiator = janek,
+                Receiver = gatsu,
+                Response = DefenderResponse.GetHit,
+                Skill = SkillType.Knockback,
+                Effects = new SkillEffects (new List<IEffect> { new Shove(MathUtils.CardinalDirection.E, map) })
+            };
+
+            _phaseAnimator = GetComponent<FightPhaseAnimator>();
+            _unitManager = CombatObjects.GetUnitManager();
+            StartCoroutine(RunTest());
+        }
+
+        IEnumerator RunTest() {
+            yield return new WaitForSeconds(2);
+
+            var liat = _unitManager.GetUnitByName("Janek");
+            liat.InCombat = true;
+            liat.Facing = MathUtils.CardinalDirection.E;
+
+            var gatsu = _unitManager.GetUnitByName("Soldier");
+            gatsu.InCombat = true;
+            gatsu.Facing = MathUtils.CardinalDirection.W;
+
+            yield return new WaitForSeconds(1);
+            yield return StartCoroutine(_phaseAnimator.Animate(_firstPhase));
+        }
+    }
+}
