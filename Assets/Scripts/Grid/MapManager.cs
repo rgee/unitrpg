686091a4ab7d5b0ab7bc92f1407398bbe8463fce
Ryan Gee <ryan.cv.gee@gmail.com>
2016-04-19 16:02:22 -0400

    make the map function work even if the map is offset

--- Assets/Scripts/Grid/MapManager.cs
+++ Assets/Scripts/Grid/MapManager.cs
@@ -12,14 +12,16 @@ namespace Grid {
         }
 
         public Vector3 GetSnappedGridPosition(Vector3 position) {
-            var totalWidth = Width*GridSize;
-            var totalHeight = Height*GridSize;
+            var maxX = transform.position.x + (Width*GridSize);
+            var maxY = transform.position.y + (Height*GridSize);
+            var minX = transform.position.x;
+            var minY = transform.position.y;
             var offset = Mathf.FloorToInt(GridSize/2.0f);
 
-            var gridX = Math.Floor(MathUtils.MapRange(0, totalWidth, 0, Width, position.x + offset));
-            var gridY = Math.Floor(MathUtils.MapRange(0, totalHeight, 0, Height, position.y + offset));
+            var gridX = Math.Floor(MathUtils.MapRange(minX, maxX, 0, Width, position.x + offset));
+            var gridY = Math.Floor(MathUtils.MapRange(minY, maxY, 0, Height, position.y + offset));
 
-            return new Vector3((float)gridX*GridSize, (float)gridY*GridSize, position.z);
+            return new Vector3((float)gridX*GridSize+transform.position.x, (float)gridY*GridSize+transform.position.y, position.z);
 
         }
 
