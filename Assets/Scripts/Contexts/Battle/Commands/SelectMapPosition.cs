3e4fbc1125c4e834b8025e475a22d9a62285d3ed
Ryan Gee <ryan.cv.gee@gmail.com>
2016-04-30 16:14:27 -0400

    IMplement the click handler for selecting units

--- Assets/Scripts/Contexts/Battle/Commands/SelectMapPosition.cs
+++ Assets/Scripts/Contexts/Battle/Commands/SelectMapPosition.cs
@@ -1,5 +1,6 @@
 ﻿using Contexts.Battle.Models;
 using Contexts.Battle.Signals;
+using Models.Fighting.Characters;
 using strange.extensions.command.impl;
 using UnityEngine;
 
@@ -16,9 +17,13 @@ namespace Contexts.Battle.Commands {
 
         public override void Execute() {
             var state = BattleViewModel.State;
+            var combatant = BattleViewModel.Map.GetAtPosition(Position);
             if (state == BattleUIState.SelectingUnit) {
-                // Mark the unit at Position as selected, change the battle state.
-                UnitSelected.Dispatch();
+                if (combatant.Army == ArmyType.Friendly) {
+                    // Mark the unit at Position as selected, change the battle state.
+                    BattleViewModel.SelectedCombatant = combatant;
+                    UnitSelected.Dispatch();
+                }
             } else if (state == BattleUIState.SelectingMoveLocation) {
                 // Make the unit move
             } else if (state == BattleUIState.SelectingAttackTarget) {
--- Assets/Scripts/Contexts/Battle/Models/BattleViewState.cs
+++ Assets/Scripts/Contexts/Battle/Models/BattleViewState.cs
@@ -1,5 +1,15 @@
-﻿namespace Contexts.Battle.Models {
+﻿using Models.Fighting;
+using Models.Fighting.Battle;
+using Models.Fighting.Maps;
+
+namespace Contexts.Battle.Models {
     public class BattleViewState {
         public BattleUIState State { get; set; }
+
+        public IBattle Battle { get; set; }
+
+        public IMap Map { get; set; }
+
+        public ICombatant SelectedCombatant { get; set; }
     }
 }
