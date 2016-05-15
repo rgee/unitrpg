841fd93081f119e0e9921ac083efa433873fe106
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-14 22:20:23 -0400

    Start sketching out the public API of the CombatantView

--- Assets/Scripts/Contexts/Battle/Views/CombatantView.cs
+++ Assets/Scripts/Contexts/Battle/Views/CombatantView.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Combat;
 using Contexts.Battle.Utilities;
 using Models.Fighting.Characters;
 using strange.extensions.mediation.impl;
+using strange.extensions.signal.impl;
 using UnityEngine;
 
 namespace Contexts.Battle.Views {
     [RequireComponent(typeof(FightPhaseAnimator))]
     [RequireComponent(typeof(CombatantController))]
+    [RequireComponent(typeof(CombatantAnimator))]
     public class CombatantView : View {
 
         public string CharacterName;
@@ -18,14 +21,48 @@ namespace Contexts.Battle.Views {
         public MathUtils.CardinalDirection Facing = MathUtils.CardinalDirection.S;
         public float SecondsPerSquare = 0.3f;
 
-        private FightPhaseAnimator _phaseAnimator;
-        private CombatantController _controller;
-
-        void Awake() {
-            _phaseAnimator = GetComponent<FightPhaseAnimator>();
-            _controller = GetComponent<CombatantController>();
+        public Signal DeathSignal {
+            get { return _animator.DeathSignal; }
         }
 
+        public Signal AttackCompleteSignal {
+            get { return _animator.AttackCompleteSignal; }
+        }
+
+        public Signal DodgeCompleteSignal {
+            get { return _animator.DodgeCompleteSignal; }
+        }
+
+        public Signal AttackConnectedSignal {
+            get { return _animator.AttackConnectedSignal; }
+        }
+
+        private FightPhaseAnimator _phaseAnimator;
+        private CombatantController _controller;
+        private CombatantAnimator _animator;
+
+        void Awake() {
+            base.Awake();
+
+            _phaseAnimator = GetComponent<FightPhaseAnimator>();
+            _controller = GetComponent<CombatantController>();
+            _animator = GetComponent<CombatantAnimator>();
+        }
+
+        public IEnumerator DoAttack() {
+            var attackComplete = false;
+            var onComplete = new Action(() => {
+                attackComplete = true;
+            });
+
+            _animator.AttackCompleteSignal.AddOnce(onComplete);
+            State = CombatantState.Attacking;
+
+            while (!attackComplete) {
+                yield return new WaitForEndOfFrame();
+            }
+            State = CombatantState.CombatReady;
+        }
 
         public IEnumerator FollowPath(IList<Vector3> path, MapDimensions dimensions) {
             return _controller.FollowPath(path, dimensions);
