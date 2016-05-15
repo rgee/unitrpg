ab0917de7f6fe23bb2b4f7a775776721a7e736aa
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-15 14:19:59 -0400

    Add logging to the fight executor

--- Assets/Scripts/Models/Fighting/Execution/FightExecutor.cs
+++ Assets/Scripts/Models/Fighting/Execution/FightExecutor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Models.Fighting.Execution {
     public class FightExecutor {
@@ -14,6 +16,13 @@ namespace Models.Fighting.Execution {
             phases.ForEach(phase => {
                 var receiver = phase.Receiver;
                 var effects = phase.Effects.ReceiverEffects;
+                var damage = phase.Effects.GetDefenderDamage();
+                var initiator = phase.Initiator;
+                var maxHealth = receiver.GetAttribute(Attribute.AttributeType.Health).Value;
+                var remainingHealth = Math.Max(0, receiver.Health - damage);
+                Debug.LogFormat("{0} did {1} damage to {2}.\nHP: {3}/{4}", initiator.Id, damage, receiver.Id, 
+                    remainingHealth, maxHealth);
+
                 effects.ForEach(effect => {
                     effect.Apply(receiver);
                 });
