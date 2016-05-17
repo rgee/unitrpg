32025252d1a5cf4d1949f974fedbcbc8a1074e49
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-17 16:51:29 -0400

    add camera view to control movement

---
+++ Assets/Scripts/Contexts/Battle/Views/CameraView.cs
@@ -0,0 +1,20 @@
+﻿using strange.extensions.mediation.impl;
+using UnityEngine;
+
+namespace Contexts.Battle.Views {
+    public class CameraView : View {
+        public float Speed = 300;
+        public bool UserControlEnabled = true;
+
+        public void Update() {
+            if (UserControlEnabled) {
+                var dt = Time.deltaTime;
+                var hSpeed = Input.GetAxis("Horizontal")*Speed*dt;
+                var vSpeed = Input.GetAxis("Vertical")*Speed*dt;
+                var adjustment = new Vector3(hSpeed, vSpeed, 0);
+
+                transform.Translate(adjustment);
+            }
+        }
+    }
+}
