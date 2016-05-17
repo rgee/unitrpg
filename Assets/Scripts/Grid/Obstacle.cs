88212ca710d51a032e754824fd7dec4c924bd062
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-16 23:26:10 -0400

    Give obstructions a width and height

--- Assets/Scripts/Grid/Obstacle.cs
+++ Assets/Scripts/Grid/Obstacle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 namespace Grid {
     [ExecuteInEditMode]
     public class Obstacle : MonoBehaviour {
+        public int Width = 1;
+        public int Height = 1;
+
         private MapView _view;
 
         void Awake() {
@@ -17,8 +20,17 @@ namespace Grid {
             var dimensions = _view.GetDimensions();
             var gridPos = dimensions.GetGridPositionForWorldPosition(transform.position);
             var snappedPosition = dimensions.GetWorldPositionForGridPosition(gridPos);
-            var size = dimensions.TileSize;
-            Gizmos.DrawCube(snappedPosition, new Vector3(size, size, 1));
+            var gizmoWidth = Width*dimensions.TileSize;
+            var gizmoHeight = Height*dimensions.TileSize;
+
+            if (Width%2 == 0) {
+                snappedPosition.x = snappedPosition.x + dimensions.TileSize / 2.0f;
+            }
+
+            if (Height%2 == 0) {
+                snappedPosition.y = snappedPosition.y + dimensions.TileSize/2.0f;
+            }
+            Gizmos.DrawCube(snappedPosition, new Vector3(gizmoWidth, gizmoHeight, 1));
         }
     }
 }
