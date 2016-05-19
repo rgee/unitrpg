6436e3dfd1942ba24e9e47695f3ddabeb0928ab9
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-19 10:01:03 -0700

    Sketch out a DSL (esque) format for bubble menus

---
+++ Assets/Scripts/Contexts/Battle/Utilities/BubbleMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Contexts.Battle.Utilities {
+    public class BubbleMenuItem {
+        public readonly string Name;
+        public readonly int Weight;
+        public readonly HashSet<BubbleMenuItem> Children;
+
+        private BubbleMenuItem(string name, int weight, HashSet<BubbleMenuItem> children) {
+            Name = name;
+            Weight = weight;
+            Children = children;
+        }
+
+        public static BubbleMenuItem Leaf(string name, int weight) {
+            return new BubbleMenuItem(name, weight, new HashSet<BubbleMenuItem>());
+        }
+
+        public static BubbleMenuItem Branch(string name, int weight, HashSet<BubbleMenuItem> children) {
+            return new BubbleMenuItem(name, weight, children);
+        }
+    }
+}
---
+++ Assets/Scripts/Contexts/Battle/Utilities/BubbleMenuTemplates.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Contexts.Battle.Utilities {
+    public class BubbleMenuTemplates {
+        public static HashSet<BubbleMenuItem> GetContextMenuTemplate() {
+            return new HashSet<BubbleMenuItem> {
+                BubbleMenuItem.Branch("System", 0, new HashSet<BubbleMenuItem> {
+                    BubbleMenuItem.Leaf("Reset", 0),
+                    BubbleMenuItem.Leaf("Suspend", 1),
+                    BubbleMenuItem.Leaf("Options", 2),
+                    BubbleMenuItem.Leaf("Reset", 3)
+                }),
+                BubbleMenuItem.Leaf("Tactical", 1),
+                BubbleMenuItem.Leaf("Units", 2),
+                BubbleMenuItem.Branch("Range", 3, new HashSet<BubbleMenuItem> {
+                    BubbleMenuItem.Leaf("Off", 0),
+                    BubbleMenuItem.Leaf("Attack", 1),
+                    BubbleMenuItem.Leaf("Absolute", 2)
+                }),
+                BubbleMenuItem.Leaf("End Turn", 4)
+            };
+        }
+    }
+}
