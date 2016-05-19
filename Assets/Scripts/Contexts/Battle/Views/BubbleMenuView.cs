84d9fb16d7b0cd1bb6dc041120c4a2ba6610eaf7
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-19 16:22:08 -0700

    Bubble menu event handling

--- Assets/Scripts/Contexts/Battle/Views/BubbleMenuView.cs
+++ Assets/Scripts/Contexts/Battle/Views/BubbleMenuView.cs
@@ -10,6 +10,7 @@ namespace Contexts.Battle.Views {
         private readonly float _showStaggerStepSeconds = 0.1f;
         private readonly Vector2 _basisVector = new Vector2(0, 1);
         private readonly Vector3 _buttonScaleFactor = new Vector3(.25f, .25f, .25f);
+        private BubbleMenuUtils.MenuStateMachine _stateMachine;
 
         public Signal<string> ItemSelectedSignal = new Signal<string>();
 
@@ -21,5 +22,26 @@ namespace Contexts.Battle.Views {
             { 4, new List<float> { 25f, -25f, 90f, -90f } },
             { 5, new List<float> { 0f, 75f, -75f, 135f, -135f } }
         };
+
+        public void Show(HashSet<BubbleMenuItem> config) {
+            _stateMachine = BubbleMenuUtils.CreateStateMachine(config);
+            _stateMachine.SelectSignal.AddListener(Select);
+            _stateMachine.ChangeLevelSignal.AddListener(ShowLevel);
+        }
+
+        public void Hide() {
+            // transition closed
+            _stateMachine.SelectSignal.RemoveListener(Select);
+            _stateMachine.ChangeLevelSignal.RemoveListener(ShowLevel);
+        }
+
+        private void ShowLevel(string level) {
+            // transition between levels
+        }
+
+        private void Select(string item) {
+            // Just relay up
+            ItemSelectedSignal.Dispatch(item);
+        }
     }
 }
