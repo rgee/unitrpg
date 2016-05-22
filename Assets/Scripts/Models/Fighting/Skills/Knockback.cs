d49a1ad53f9bfbde2e9cbed75d2c25a41aa9d133
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-22 01:11:25 -0400

    Only one hit for knockback

--- Assets/Scripts/Models/Fighting/Skills/Knockback.cs
+++ Assets/Scripts/Models/Fighting/Skills/Knockback.cs
@@ -1,4 +1,5 @@
-﻿using Models.Fighting.Effects;
+﻿using System.Linq;
+using Models.Fighting.Effects;
 using Models.Fighting.Maps;
 
 namespace Models.Fighting.Skills {
@@ -9,14 +10,30 @@ namespace Models.Fighting.Skills {
             _map = map;
         }
 
+        protected override SkillForecast ComputeForecast(ICombatant attacker, ICombatant defender) {
+            var result = base.ComputeForecast(attacker, defender);
+
+            // Knockback only gets one hit, always
+            var hit = new SkillHit {
+                BaseDamage = DamageUtils.ComputeMeleeDamage(attacker, defender),
+                HitCount = 1 
+            };
+
+            result.Hit = hit;
+
+            return result;
+        }
+
         protected override SkillEffects ComputeEffects(SkillForecast forecast, IRandomizer randomizer) {
             var baseEffects = base.ComputeEffects(forecast, randomizer);
 
             var attacker = forecast.Attacker;
             var defender = forecast.Defender;
 
-            var direction = MathUtils.DirectionTo(attacker.Position, defender.Position);
-            baseEffects.ReceiverEffects.Add(new Shove(direction, _map));
+            if (!baseEffects.ReceiverEffects.OfType<Miss>().Any()) {
+                var direction = MathUtils.DirectionTo(attacker.Position, defender.Position);
+                baseEffects.ReceiverEffects.Add(new Shove(direction, _map));
+            }
 
             return baseEffects;
         }
