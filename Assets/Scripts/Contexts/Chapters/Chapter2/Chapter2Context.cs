e4a9dd12b8a2de2b9af84026e29570181221d49e
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-27 22:20:34 -0400

    Set up a strangeioc version of chapter 2

---
+++ Assets/Scripts/Contexts/Chapters/Chapter2/Chapter2Context.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Contexts.Battle;
+using strange.extensions.context.impl;
+using UnityEngine;
+
+namespace Assets.Contexts.Chapters.Chapter2 {
+    public class Chapter2Context : BattleContext {
+        public Chapter2Context(MonoBehaviour view) : base(view) {
+
+        }
+
+        protected override void mapBindings() {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Contexts/Chapters/Chapter2/Chapter2Root.cs
@@ -0,0 +1,9 @@
+﻿using Contexts.Battle;
+
+namespace Assets.Contexts.Chapters.Chapter2 {
+    public class Chapter2Root : BattleRoot {
+        void Awake() {
+            context = new Chapter2Context(this);
+        }
+    }
+}
