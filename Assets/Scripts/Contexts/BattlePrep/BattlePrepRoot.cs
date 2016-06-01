6be6fd05f5f9a941f1e8463f077e42db24799035
Ryan Gee <ryan.cv.gee@gmail.com>
2016-05-31 21:59:00 -0400

    Load chapter 1 before creating the context

--- Assets/Scripts/Contexts/BattlePrep/BattlePrepRoot.cs
+++ Assets/Scripts/Contexts/BattlePrep/BattlePrepRoot.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 namespace Contexts.BattlePrep {
     public class BattlePrepRoot : ContextView {
         void Awake() {
-            context = new BattlePrepContext(this);
             if (context == Context.firstContext) {
-            Instantiate(Resources.Load("Prefabs/Scenes/Chapter 1 Root"));
+                Instantiate(Resources.Load("Prefabs/Scenes/Chapter 1 Root"));
             }
+            context = new BattlePrepContext(this);
         } 
     }
 }
