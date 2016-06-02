9b42cafe74777826687ef4f735cd0288fb7aabfb
Ryan Gee <ryan.cv.gee@gmail.com>
2016-06-01 21:23:14 -0400

    Add model for eastmere and add initial inspection points

---
+++ Assets/Scripts/Contexts/Chapters/Chapter2/Models/EastmerePlazaState.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Contexts.Chapters.Chapter2.Models {
+    public class EastmerePlazaState {
+        public bool InnInspected;
+        public bool HouseInspected;
+        public bool ClinicInspected;
+    }
+}
