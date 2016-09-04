e6307d2661e3a32dc15b36a2c7c0339159276745
Ryan Gee <ryan.cv.gee@gmail.com>
2016-09-04 18:11:10 -0400

    Add a character configuration object

---
+++ Assets/Scripts/Contexts/Global/Models/CharacterConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Contexts.Global.Models {
+    public class CharacterConfiguration {
+        public readonly string Id;
+        public readonly string Name;
+
+        public CharacterConfiguration(string id, string name) {
+            Id = id;
+            Name = name;
+        }
+    }
+}
